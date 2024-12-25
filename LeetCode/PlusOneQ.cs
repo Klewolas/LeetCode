5519dcf8a5b7fdc9dd0ae4147c71eb8b58e31ae3
Berke Altıntaş <berke.altintas@hotmail.com>
2024-12-25 21:23:56 +0300

    Plus One 66

---
+++ LeetCode/PlusOneQ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    internal class PlusOneQ
+    {
+        public static void Main(string[] args)
+        {
+            var answer = PlusOne(new[] { 9,9,9,9,9,9,9,9 });
+            
+            foreach (var i in answer)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+
+        private static int[] PlusOne(int[] digits) {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9) digits[i] = 0;
+                else
+                {
+                    digits[i]++;
+                    return digits;
+                }
+            }
+            int[] firstOne = new int[] { 1 };
+            return firstOne.Concat(digits).ToArray();
+        }
+    }
+}
